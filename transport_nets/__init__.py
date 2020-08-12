78f7c98fffcf0a9229be20a0700fa580bcddfdf8
CaseyDowdle <caseyldowdle@gmail.com>
2020-08-12 13:29:32 -0400

    Made a bijector for the banana map under the bijectors folder and a function to create a transformed distribution using the bijector. Used banana bijector instead for MH-MCMC example. Changed folder name to lower case and made __init__ files to ensure correct attributes of the module are accessible.

---
+++ transport_nets/__init__.py
@@ -0,0 +1,7 @@
+from transport_nets import bijectors
+from transport_nets import Models
+from transport_nets import Probability
+
+__all__ = ['bijectors',
+           'Models',
+           'Probability']
---
+++ transport_nets/bijectors/__init__.py
@@ -0,0 +1,5 @@
+from transport_nets.bijectors.banana_map import BananaMap
+from transport_nets.bijectors.banana_map import BananaFlow
+
+__all__ = ['BananaMap',
+           'BananaFlow']      
