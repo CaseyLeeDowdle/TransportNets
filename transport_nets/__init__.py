a62caee8bf434e8c26e06d0e4aa737a6e752133a
CaseyDowdle <caseyldowdle@gmail.com>
2020-08-17 16:13:59 -0400

    Made a new directory called mcmc with the file metropolis_hastings which includes a more generalized metropolis-hastings function than the original called MetroploisHastings and a class called log_prob_model that creates a function to return the log probability of a model. Useful for evaluting a joint probability propotional to the conditional by initializing it with the y_given argument.

--- transport_nets/__init__.py
+++ transport_nets/__init__.py
@@ -1,7 +1,9 @@
 from transport_nets import bijectors
 from transport_nets import Models
 from transport_nets import Probability
+from transport_nets import mcmc
 
 __all__ = ['bijectors',
            'Models',
-           'Probability']
+           'Probability',
+           'mcmc']
