774d2f95865b8a0b67cdbbf699e76f1b457e9f37
Casey Dowdle <caseyldowdle@gmail.com>
2020-09-22 11:49:21 -0400

    Made notebooks for true map MCMC, MH MCMC with NVP learning joint distribution, and RTO-MH with NVP learning joint distribution. RTO algorithm in mcmc library.

--- transport_nets/mcmc/__init__.py
+++ transport_nets/mcmc/__init__.py
@@ -1,5 +1,7 @@
 from transport_nets.mcmc.metropolis_hastings import MetropolisHastings
 from transport_nets.mcmc.metropolis_hastings import model_log_prob
+from transport_nets.mcmc.rto_metropolis_hastings import RTO_MetropolisHastings
 
 __all__ = ['MetropolisHastings',
-           'model_log_prob']
+           'model_log_prob',
+          'RTO_MetropolisHastings']
--- transport_nets/mcmc/metropolis_hastings.py
+++ transport_nets/mcmc/metropolis_hastings.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import tensorflow_probability as tfp
 from tqdm import trange
+import time
 
 tfb = tfp.bijectors
 
@@ -28,6 +29,7 @@ def MetropolisHastings(theta,b,niters,log_prob_fn):
     samples = tf.TensorArray(tf.float32,size=int(niters+1), dynamic_size=False)
     samples = samples.write(0,theta)
     naccept= 0
+    t0 = time.time()
     for i in trange(1,niters+1):
         theta_p = theta + tf.random.normal([n_param])*b
         rho = min(1, tf.math.exp(log_prob_fn(theta_p)-log_prob_fn(theta)))
@@ -36,9 +38,10 @@ def MetropolisHastings(theta,b,niters,log_prob_fn):
             naccept += 1
             theta = theta_p
         samples = samples.write(i,theta)
+    t1 = time.time()
     acceptance_rate = naccept/niters
     
-    return acceptance_rate, samples.stack()
+    return samples.stack().numpy(),acceptance_rate,t1-t0
 
 class model_log_prob():
     """Flexible way to create a function that returns the 
@@ -60,7 +63,7 @@ class model_log_prob():
             conditional probability you want to sample."""
         
         if y_given != None:
-            assert tf.rank(y_given) == 1
+            assert tf.rank(y_given).numpy() == 1
         self.model = model
         if permute == None:
             self.permute = tfb.Identity()
@@ -76,7 +79,7 @@ class model_log_prob():
         else:
             self.y_given = y_given
             def log_prob(x):
-                xy = tf.stack([x,self.y_given],axis=1)
+                xy = tf.reshape(tf.concat([x,self.y_given],axis=-1),shape=[1,-1])
                 xy = self.permute.forward(xy)
                 return self.model.log_prob(xy)
             self.log_prob = log_prob
