40d924f82e1732978445cea25b2c84b388d7ca55
CaseyDowdle <caseyldowdle@gmail.com>
2020-10-06 03:01:40 -0400

    ComposedFlow under models, used to train the preconditioner and conditionally sample from the composed normalizing flows. Example using NVP in banana example. MNIST digit inpainting using MGAN in mnist example.

--- transport_nets/__init__.py
+++ transport_nets/__init__.py
@@ -2,10 +2,8 @@ from transport_nets import bijectors
 from transport_nets import distributions
 from transport_nets import mcmc
 from transport_nets import models
-from transport_nets import training
 
 __all__ = ['bijectors',
            'distributions',
            'mcmc',
-           'models',
-           'training']
+           'models']
